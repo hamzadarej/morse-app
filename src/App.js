ebe2f3fcd30a4a842a35234f32442cd571af8995
hamza <darejhamza@gmail.com>
2021-06-02 03:37:58 +0200

    morse-app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import React, {useState} from "react";
+import Results from "./components/Morse";
+
 function App() {
+  const [userInput, setUserInput] = useState("");
+  function changeHandel(e){
+    setUserInput(e.target.value);
+  }
+  
+     
   return <div className="App">
-    <h1>Morse Generator</h1>
+    <h1>Morse Convertor</h1>
+    <div className="boxes">
+    <input type="text" placeholder="Enter your message" value={userInput} onChange={changeHandel}></input>
+    <div className="box">
+      <h3>your morsecode is :</h3>
+      <div className="results" >
+        <Results userInput={userInput}/>
+      </div>
+      </div>
+    </div>
   </div>;
 }
 
